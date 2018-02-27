9dcaff0a708ae7a4bf4065f6b85b271375efa89b
GuillaumeCz <guillaume.couzy@gmail.com>
2018-02-27 15:36:37 +0100

    Add dynamic year to URL and UTF-8 encoding

--- r2c/r2c.py
+++ r2c/r2c.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 
 now = datetime.datetime.now()
 week = format(datetime.date(now.year, now.month, now.day).isocalendar()[1], '02d')
-url = 'http://www.r2c-restauration.fr/suggestion-de-la-semaine-s'+week+'-2018'
+url = 'http://www.r2c-restauration.fr/suggestion-de-la-semaine-s'+week+'-'+str(now.year)
 req = requests.get(url).text
 soup = BeautifulSoup(req, "lxml")
 
@@ -21,13 +21,13 @@ def display_menu():
     entrees = rows[0:8]
     plats = rows[9:21]
 
-    menu += "--> "+headers[0].string.encode('ascii', 'ignore') + "\n"
+    menu += "--> "+headers[0].string.encode('UTF-8', 'ignore') + "\n"
     for entree in entrees:
-        menu += "\t"+entree.string.encode('ascii', 'ignore')+ "\n"
+        menu += "\t"+entree.string.encode('UTF-8', 'ignore')+ "\n"
 
-    menu += "--> "+headers[1].string.encode('ascii', 'ignore') + "\n"
+    menu += "--> "+headers[1].string.encode('UTF-8', 'ignore') + "\n"
     for plat in plats:
-        menu += "\t"+plat.string.encode('ascii', 'ignore')+"\n"
+        menu += "\t"+plat.string.encode('UTF-8', 'ignore')+"\n"
 
     print menu 
 
