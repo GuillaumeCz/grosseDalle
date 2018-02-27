b06b18d9d08ed395ce48044ca9d620a20707882d
GuillaumeCz <guillaume.couzy@gmail.com>
2018-02-27 13:08:19 +0100

    Setup + code

---
+++ r2c/r2c.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.r2c-restauration.fr/suggestion-de-la-semaine-s09-2018'
+req = requests.get(url).text
+soup = BeautifulSoup(req, "lxml")
+
+def display_menu():
+    menu = ""
+    headers = soup.find_all("h3")
+    rows = soup.find("table").find_all("td")
+
+    entrees = rows[0:8]
+    plats = rows[9:21]
+
+    menu += "--> "+headers[0].string.encode('ascii', 'ignore') + "\n"
+    for entree in entrees:
+        menu += "\t"+entree.string.encode('ascii', 'ignore')+ "\n"
+
+    menu += "--> "+headers[1].string.encode('ascii', 'ignore') + "\n"
+    for plat in plats:
+        menu += "\t"+plat.string.encode('ascii', 'ignore')+"\n"
+
+    print menu 
+
+def main(argv=None):
+    display_menu()
+
+if __name__ == '__main__':
+    main()
+
---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+  name="GrosseDalle",
+  version="0.0.1",
+  description="Pour ceux qui ont fait et ne peuvent attendre d'etre devant Le Champ de L'étoile pour attiser l'appetit",
+  install_requires=[
+    'requests',
+    'bs4'
+  ],
+  packages=find_packages(),
+  entry_points={
+    'console_scripts': [
+      'r2c=r2c.r2c:main'
+    ]
+  }
+)
