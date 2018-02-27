12998c68c61ad7e1137d6fe59c95c0e38770aded
GuillaumeCz <guillaume.couzy@gmail.com>
2018-02-27 15:49:48 +0100

    Flake8 modif

--- r2c/r2c.py
+++ r2c/r2c.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 
 import datetime
-import sys
 import requests
 from bs4 import BeautifulSoup
 
 
 now = datetime.datetime.now()
-week = format(datetime.date(now.year, now.month, now.day).isocalendar()[1], '02d')
-url = 'http://www.r2c-restauration.fr/suggestion-de-la-semaine-s'+week+'-'+str(now.year)
+week = datetime.date(now.year, now.month, now.day).isocalendar()[1]
+week = format(week, '02d')
+
+url = 'http://www.r2c-restauration.fr/suggestion-de-la-semaine-s' \
+        + week + '-' + str(now.year)
 req = requests.get(url).text
 soup = BeautifulSoup(req, "lxml")
 
@@ -21,19 +23,20 @@ def display_menu():
     entrees = rows[0:8]
     plats = rows[9:21]
 
-    menu += "--> "+headers[0].string.encode('UTF-8', 'ignore') + "\n"
+    menu += "--> " + headers[0].string.encode('UTF-8', 'ignore') + "\n"
     for entree in entrees:
-        menu += "\t"+entree.string.encode('UTF-8', 'ignore')+ "\n"
+        menu += "\t" + entree.string.encode('UTF-8', 'ignore') + "\n"
 
-    menu += "--> "+headers[1].string.encode('UTF-8', 'ignore') + "\n"
+    menu += "--> " + headers[1].string.encode('UTF-8', 'ignore') + "\n"
     for plat in plats:
-        menu += "\t"+plat.string.encode('UTF-8', 'ignore')+"\n"
+        menu += "\t" + plat.string.encode('UTF-8', 'ignore') + "\n"
 
-    print menu 
+    print menu
+
 
 def main(argv=None):
     display_menu()
 
+
 if __name__ == '__main__':
     main()
-
